32273e525b3af91e26e2185b750a1444e0a0dd21
Frenco <hey@frenco.dev>
2021-08-22 10:25:06 +0630

    feat: add bracket validator to REPL

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,8 +1,28 @@
 use interpreter::parse::Parser;
+use rustyline::validate::{
+    MatchingBracketValidator, ValidationContext, ValidationResult, Validator,
+};
 use rustyline::{error::ReadlineError, Editor};
+use rustyline_derive::{Completer, Helper, Highlighter, Hinter};
+
+#[derive(Completer, Helper, Highlighter, Hinter)]
+struct InputValidator {
+    brackets: MatchingBracketValidator,
+}
+
+impl Validator for InputValidator {
+    fn validate(&self, ctx: &mut ValidationContext) -> rustyline::Result<ValidationResult> {
+        self.brackets.validate(ctx)
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut rl = Editor::<()>::new();
+    let h = InputValidator {
+        brackets: MatchingBracketValidator::new(),
+    };
+
+    let mut rl = Editor::new();
+    rl.set_helper(Some(h));
     if rl.load_history("history.txt").is_err() {
         std::fs::write("history.txt", "")?;
     }
@@ -11,12 +31,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let readline = rl.readline("> ");
         match readline {
             Ok(line) => {
-                rl.add_history_entry(line.as_str());
+                if !line.trim().is_empty() {
+                    rl.add_history_entry(line.as_str());
 
-                let mut parser = Parser::new(&line);
-                let ast = parser.parse()?;
+                    let mut parser = Parser::new(&line);
+                    let ast = parser.parse()?;
 
-                println!("{:#?}", ast);
+                    println!("{:#?}", ast);
+                }
             }
             Err(ReadlineError::Interrupted | ReadlineError::Eof) => break,
             Err(error) => {
